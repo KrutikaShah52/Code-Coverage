d38d6bf8d33886a9c68e43039fa8a5274b6b8a53
Krutika Shah <31092884+KrutikaShah52@users.noreply.github.com>
2018-04-02 12:00:06 +0530

    Update Palindrome.java
    changes for printing satement..

--- src/main/java/com/test/Jacoco_Maven_Example/PalindromeExample.java
+++ src/main/java/com/test/Jacoco_Maven_Example/PalindromeExample.java
@@ -4,13 +4,13 @@ public class PalindromeExample {
 
 	public boolean isPalindrome(String inputString) {
 	    if (inputString.length() == 0) {
-		System.println("If portion............");
+		System.out.println("If portion............");
 	        return true;
 	    } else {
 	        char firstChar = inputString.charAt(0);
 	        char lastChar = inputString.charAt(inputString.length() - 1);
 	        String mid = inputString.substring(1, inputString.length() - 1);
-		System.println("Else part..................");
+		System.out.println("Else part..................");
 	        return (firstChar == lastChar) && isPalindrome(mid);
 	    }
 	}
