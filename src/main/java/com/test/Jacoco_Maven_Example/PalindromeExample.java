3a2521bf9bebd85df22cda9fdad48128bc811ac5
Krutika Shah <31092884+KrutikaShah52@users.noreply.github.com>
2018-04-24 16:18:07 +0530

    Update the file for adding function Jacoco

--- src/main/java/com/test/Jacoco_Maven_Example/PalindromeExample.java
+++ src/main/java/com/test/Jacoco_Maven_Example/PalindromeExample.java
@@ -18,9 +18,10 @@ public class PalindromeExample {
 	        return (firstChar == lastChar) && isPalindrome(mid);
 	    }
 	}
-// 	public void add()
-// 	{
-// 		System.out.println("Adding something");
-// 	}
+	public void add()
+	{
+		System.out.println("Adding something");
+		System.out.println("Adding something new");
+	}
 	
 }
